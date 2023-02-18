7e06ee0ed771fd97b61e43d09e326d0bcecf7b80
nicoleivo <cucchiettinicolas@gmail.com>
2023-02-18 18:21:30 +0100

    last files added

---
+++ src/components/Note.tsx
@@ -0,0 +1,52 @@
+import { Badge, Button, Col, Row, Stack } from 'react-bootstrap';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import { Link, useNavigate } from 'react-router-dom';
+import { useNotes } from './NoteLayout';
+
+type NoteProps = {
+  onDelete: (id: string) => void;
+};
+
+export function Note({ onDelete }: NoteProps) {
+  const note = useNotes();
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Row className='align-items-center mb-4'>
+        <Col>
+          <h1>{note.title}</h1>
+          {note.tags.length > 0 && (
+            <Stack gap={1} direction='horizontal' className='flex-wrap'>
+              {note.tags.map((tag) => (
+                <Badge className='text-truncate' key={tag.id}>
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Col>
+        <Col xs='auto'>
+          <Stack gap={2} direction='horizontal'>
+            <Link to={`/${note.id}/edit`}>
+              <Button variant='primary'>Edit</Button>
+            </Link>
+            <Button
+              variant='outline-danger'
+              onClick={() => {
+                onDelete(note.id);
+                navigate('..');
+              }}
+            >
+              Delete
+            </Button>
+            <Link to='..'>
+              <Button variant='outline-secondary'>Back</Button>
+            </Link>
+          </Stack>
+        </Col>
+      </Row>
+      <ReactMarkdown>{note.markdown}</ReactMarkdown>
+    </>
+  );
+}
